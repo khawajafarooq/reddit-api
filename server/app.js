ae02084b0414240575c5fbf48098b32e6385294d
Khawaja Farooq <kfarooqa@gmail.com>
2018-01-16 11:03:57 +0300

    setup for session

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import bodyParser from 'body-parser'
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import passport from 'passport';
 import routes from './routes';
 
 mongoose.connect('mongodb://localhost:27017/reddit', () => {
@@ -9,8 +12,23 @@ mongoose.connect('mongodb://localhost:27017/reddit', () => {
 
 const app = express();
 
+// Routes
+app.use('/api', routes);
+
 // Middleware
 app.use(bodyParser.json());
-app.use('/api', routes);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Session
+app.use(session({
+  secret: 'secret',
+  saveUninitialized: true,
+  resave: true
+}));
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
 
 export default app;
