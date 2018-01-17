4e047f8e24557598b2965daa9211f5fc30ba0ab3
Khawaja Farooq <kfarooqa@gmail.com>
2018-01-17 07:27:15 +0300

    improving server structure

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,10 @@
 import express from 'express';
 
 // Controller imports
-import basicController from './controllers/basicController';
-import userController from './controllers/userController';
-import postController from './controllers/postController';
-import commentController from './controllers/commentController';
+import basicController from './../controllers/basicController';
+import userController from './../controllers/userController';
+import postController from './../controllers/postController';
+import commentController from './../controllers/commentController';
 
 const routes = express();
 
